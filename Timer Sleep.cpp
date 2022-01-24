d24b4a578ca30cd9839192ac87f2af912a029106
Ellieka <42927706+tmdgpdhkdehd@users.noreply.github.com>
2022-01-24 16:51:00 +0900

    Create Timer Sleep.cpp

---
+++ Timer Sleep.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <Windows.h>
+
+
+int main()
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		a++;
+
+		Sleep(1000);
+
+		printf("%d \n", a);
+	}
+
+	//printf("%s \n", s1);
+}
