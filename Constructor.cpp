a87ca08e41a32310544de0c4edbdbfbdc35947aa
Ellieka <42927706+tmdgpdhkdehd@users.noreply.github.com>
2022-01-24 09:16:34 +0900

    Create Constructor.cpp

---
+++ Constructor.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "utils.h"
+
+//struct Player {
+//    char name[20];
+//    int level;
+//    int hp;
+//};
+
+//typedef struct Player {
+//    char name[20];
+//    int level;
+//    int hp;
+//} _Player;
+
+
+class Player {
+private:
+    char name[20];
+    int level;
+    int hp;
+public:
+    Player()
+    {
+        strcpy(name, "aaa");
+        level = 10;
+        hp = 100;
+    }
+
+    int GetLevel() { return level; }
+    bool SetLevel(int newLevel)
+    {
+        if (newLevel > 0 && newLevel <= 300)
+        {
+            level = newLevel;
+            return true;
+        }
+    }
+    void LevelUp() { level++; }
+    void IncHP(int amount) { hp += amount; }
+};
+
+int  main()
+{
+    Player p1;
+    //p1.SetLevel(20);
+
+    printf("%d \n", p1.GetLevel());
+    //strcpy(p1.name, "abc");
+}
+
