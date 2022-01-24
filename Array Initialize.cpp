41539b0550d2a53028ba915f800ebc3a2a0f4ca5
Ellieka <42927706+tmdgpdhkdehd@users.noreply.github.com>
2022-01-24 16:47:39 +0900

    Create Array Initialize.cpp

---
+++ Array Initialize.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	int nArray[7] = { 1,2,3,4,5,6,7 };
+	printf("%d \n", nArray[3]);
+
+	int nArray2[10] = { 0, };
+	printf("%d \n", nArray2[3]);
+
+	// int nArray3[]; // error
+
+	printf("int size %d \n", sizeof(int));
+	printf("%d \n", sizeof(nArray));
+	printf("%d \n", sizeof(nArray2));
+}
