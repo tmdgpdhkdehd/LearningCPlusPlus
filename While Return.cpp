ba1729207aa24b971dd444356f3429fe74b8c9c2
Ellieka <42927706+tmdgpdhkdehd@users.noreply.github.com>
2022-01-24 09:15:05 +0900

    Create While Return.cpp

---
+++ While Return.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+	int a = 1;
+
+	while (a <= 10)
+	{
+		a++;
+		if ((a % 3) == 0)
+			return 0;
+
+		printf("a = %d \n", a);
+	}
+
+	//do
+	//{
+	//	a++;
+	//	if ((a % 3) == 0)
+	//		continue;
+
+	//	printf("a = %d \n", a);
+
+	//} while (a <= 10);
+}
