61a62c12def9f80db49100c6680403067b96c191
Ellieka <42927706+tmdgpdhkdehd@users.noreply.github.com>
2022-01-24 16:48:11 +0900

    Create Call by Referrence.cpp

---
+++ Call by Referrence.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void add(int n1, int n2, int& sum);
+
+int main()
+{
+	int n1 = 2;
+	int n2 = 3;
+	int sum;
+
+	add(n1, n2, sum);
+
+	printf("%d \n", sum);
+}
+
+void add(int n1, int n2, int& sum)
+{
+	sum = n1 + n2;
+}
